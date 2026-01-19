95a4bf3d57cde6dc5c0efdfd5ccdd06084f3cd06
usedigi-matheuscadenassi <matheus.cadenassi@usedigi.com.br>
2026-01-18 21:56:26 -0300

    Feat: Cria componente Heading

---
+++ src/components/Heading/index.tsx
@@ -0,0 +1,16 @@
+import type React from 'react';
+import style from './style.module.css';
+
+type HeadingProps = {
+  children: React.ReactNode
+};
+
+export function Heading({children}: HeadingProps) {
+
+  return (
+    <>
+      <h1 className={style.heading}>{children}</h1>
+    </>
+  );
+
+}
