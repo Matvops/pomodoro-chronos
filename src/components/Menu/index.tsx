4b35ce9c6dce3a61358042a722fb7db082928b00
usedigi-matheuscadenassi <matheus.cadenassi@usedigi.com.br>
2026-01-26 20:48:37 -0300

    Feat: Adiciona atualização de tema

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,15 +1,41 @@
-import { HistoryIcon, HomeIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import { HistoryIcon, HomeIcon, MoonIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import style from './style.module.css';
+import { useEffect, useState, type MouseEvent } from 'react';
+
+type Themes = 'dark' | 'light';
 
 export function Menu() {
+
+  const [theme, setTheme] = useState<Themes>(() => {
+    return localStorage.getItem('theme') == 'light' ? 'light' : 'dark';
+  });
+
+  const iconThemes = {
+    dark: <SunIcon />,
+    light: <MoonIcon />
+  };
+  
+  const handleClick = (event: MouseEvent) => {
+    event.preventDefault();
+    setTheme(prevElement => prevElement == 'dark' ? 'light' : 'dark');
+  }
+
+  useEffect(() => {
+
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+
+  }, [theme]);
 
   return (
     <>
       <nav className={style.menu}>
-        <a href="#" className={style.menuLink}><HomeIcon /></a>
-        <a href="#" className={style.menuLink}><HistoryIcon /></a>
-        <a href="#" className={style.menuLink}><SettingsIcon /></a>
-        <a href="#" className={style.menuLink}><SunIcon /></a>
+        <a href="#" className={style.menuLink} aria-label='home' title='home'><HomeIcon /></a>
+        <a href="#" className={style.menuLink} aria-label='history' title='history'><HistoryIcon /></a>
+        <a href="#" className={style.menuLink} aria-label='settings' title='settings'><SettingsIcon /></a>
+        <a href="#" className={style.menuLink} onClick={handleClick} aria-label='alther theme' title='alther theme'>
+          {iconThemes[theme]}
+        </a>
       </nav>
     </>
   );
