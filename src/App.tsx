a7f4e5d9ee7e9564daee90781e5236b981554519
usedigi-matheuscadenassi <matheus.cadenassi@usedigi.com.br>
2026-01-26 21:00:58 -0300

    Feat: Adiciona Page Home ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,12 @@
-import { Container } from "./components/Container";
-import { Logo } from "./components/Logo";
-import { Menu } from "./components/Menu";
-
 import "./styles/theme.css";
 import "./styles/global.css";
-import { CountDown } from "./components/CountDown";
-import { FormHome } from "./components/FormHome";
-import { Footer } from "./components/Footer";
+import { Home } from "./pages/Home";
 
 export function App() {
 
   return (
     <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <FormHome />
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
+      <Home />
     </>
   )
 
