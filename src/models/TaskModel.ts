8a1e28fdd18e920c9b413ea088b7c2ed36fe228d
usedigi-matheuscadenassi <matheus.cadenassi@usedigi.com.br>
2026-01-27 21:46:22 -0300

    Feat: Cria TaskModel

---
+++ src/models/TaskModel.ts
@@ -0,0 +1,11 @@
+import type { TaskModelState } from "./TaskModelState";
+
+export type TaskModel = {
+    id: string,
+    name: string,
+    durationInMinutes: number,
+    startDate: number,
+    completeDate: number | null,
+    interruptDate: number | null,
+    type: keyof TaskModelState['config']
+};
