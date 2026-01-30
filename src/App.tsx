257f9c7f1aba5b824d20b31dfc6765d375711af9
usedigi-matheuscadenassi <matheus.cadenassi@usedigi.com.br>
2026-01-30 19:56:41 -0300

    Feat: Adiciona objeto global state ao App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 import { Home } from "./pages/Home";
+import { useState } from "react";
+import type { TaskModelState } from "./models/TaskModelState";
+
+const initialState: TaskModelState = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    work: 25,
+    rest: 15,
+    shortRest: 5
+  }
+};
 
 export function App() {
 
+  const [state, setState] = useState<TaskModelState>(initialState);
+
   return (
     <>
-      <Home />
+      <Home
+        state={state}
+        setState={setState}
+      />
     </>
   )
 
