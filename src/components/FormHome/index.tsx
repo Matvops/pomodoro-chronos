c0fa2b1048c7d661cfe6e2d24030748332c27e1d
usedigi-matheuscadenassi <matheus.cadenassi@usedigi.com.br>
2026-02-17 00:01:55 -0300

    Feat: Adiciona lógica para interromper task

--- src/components/FormHome/index.tsx
+++ src/components/FormHome/index.tsx
@@ -1,9 +1,9 @@
-import { PlayCircleIcon } from 'lucide-react';
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
 import { ButtonDefault } from '../ButtonDefault';
 import { Cycles } from '../Cycles';
 import { InputDefault } from '../InputDefault';
 import style from './style.module.css';
-import { useContext, useState, type FormEvent } from 'react';
+import { useContext, useState, type FormEvent, type MouseEvent } from 'react';
 import type { TaskModel } from '../../models/TaskModel';
 import { TaskContext } from '../../contexts/TaskContext';
 import { getNextCycle } from '../../utils/getNextCycle';
@@ -54,6 +54,29 @@ export function FormHome() {
     });
   }
 
+  function handleInterruptTask(e: MouseEvent) {
+
+    e.preventDefault();
+
+    setTask(prevState => {
+
+      return {
+        ...prevState,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: "00:00",
+        tasks: prevState.tasks.map(task => {
+
+          if(task.id == prevState.activeTask?.id) {
+            return {...task, interruptDate: Date.now()}
+          }
+
+          return task;
+        })
+      }
+    })
+  }
+
   return (
     <form onSubmit={handleSubmit} className={style.form} action='#'>
       <div className={style.formRow}>
@@ -64,6 +87,7 @@ export function FormHome() {
           placeholder='Digite algo...'
           value={taskName}
           onChange={e => setTaskName(e.target.value)}
+          disabled={!!task.activeTask}
         />
       </div>
 
@@ -78,9 +102,29 @@ export function FormHome() {
       )}
 
       <div className={style.formRow}>
-        <ButtonDefault color='green' >
-          <PlayCircleIcon />
-        </ButtonDefault>
+        {!task.activeTask ? (
+          <ButtonDefault 
+            key={1}
+            color='green' 
+            aria-label='Iniciar tarefa'
+            title='Iniciar tarefa'
+            type='submit'
+          >
+            <PlayCircleIcon/>
+          </ButtonDefault>
+        ) : 
+        (
+          <ButtonDefault 
+            key={2}
+            color='red' 
+            aria-label='Interromper a tarefa'
+            title='Interromper tarefa'
+            type='button'
+            onClick={handleInterruptTask}
+          >
+            <StopCircleIcon />
+          </ButtonDefault>
+        )}
       </div>
     </form>
   );
