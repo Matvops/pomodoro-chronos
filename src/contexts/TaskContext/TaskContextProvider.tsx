ec1203a5c49059ad5ac988cb6d37e3b3ea99d311
Matheus <matheusmartinscia@gmail.com>
2026-03-03 21:24:39 -0300

    Feat: Adiciona instância do worker e atualização do contador

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -2,6 +2,8 @@ import { useEffect, useReducer } from "react"
 import { TaskContext } from "."
 import { initialState } from "./initialTaskState"
 import { taskReducer } from "./taskReducer"
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager"
+import { TaskActionTypes } from "./taskActions"
 
 type TaskContextProviderProps = {
   children: React.ReactNode
@@ -10,10 +12,32 @@ type TaskContextProviderProps = {
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
   const [task, dispatch] = useReducer(taskReducer, initialState);
+  
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage(event => {
+
+    const countDownSeconds = event.data;
+
+    if(countDownSeconds <= 0) {
+      dispatch({ type: TaskActionTypes.COMPLETE_TASK })
+      worker.terminate();
+    } else {
+      dispatch({ type: TaskActionTypes.COUNT_DOWN, payload: { secondsRemaining: countDownSeconds } })
+    }
+
+  })
+
 
   useEffect(() => {
+    if(!task.activeTask) {
+      worker.terminate();
+    }
+
+    worker.postMessage(task);
+
     console.log(task);
-  }, [task]);
+  }, [worker, task]);
 
   return (
     <TaskContext.Provider value={{task, dispatch}}>
