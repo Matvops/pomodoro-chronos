d8f92e89de8dd44cb072dedaf68875ef4186fcd2
Matheus <matheusmartinscia@gmail.com>
2026-03-10 20:55:57 -0300

    Feat: Adiciona lógica para tocar som e carregar som

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from "react"
+import { useEffect, useReducer, useRef } from "react"
 import { TaskContext } from "."
 import { initialState } from "./initialTaskState"
 import { taskReducer } from "./taskReducer"
 import { TimerWorkerManager } from "../../workers/TimerWorkerManager"
 import { TaskActionTypes } from "./taskActions"
+import { loadBeep } from "../../utils/loadBeep"
 
 type TaskContextProviderProps = {
   children: React.ReactNode
@@ -13,6 +14,8 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
   const [task, dispatch] = useReducer(taskReducer, initialState);
   
+  const beep = useRef<() => void | null>(null);
+
   const worker = TimerWorkerManager.getInstance();
 
   worker.onmessage(event => {
@@ -38,6 +41,18 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
     console.log(task);
   }, [worker, task]);
+
+  useEffect(() => {
+
+    if(task.activeTask && beep.current === null) {
+      beep.current = loadBeep();
+      beep.current();
+    } else {
+      beep.current = null;
+    }
+
+
+  }, [task.activeTask]);
 
   return (
     <TaskContext.Provider value={{task, dispatch}}>
