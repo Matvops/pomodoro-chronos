b93d444464ed30f89c4fa731d543e8fd1d18bda2
Matheus <matheusmartinscia@gmail.com>
2026-05-06 22:16:45 -0300

    Feat: Cria função para excluir tasks

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -9,17 +9,26 @@ import { TaskContext } from "../../contexts/TaskContext";
 import { formatDate } from "../../utils/formatDate";
 import { getTaskStatus } from "../../utils/getTaskStatus";
 import { sortTasks } from "../../utils/sortTasks";
+import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
 
 export function History() {
 
-  const { task } = useContext(TaskContext);
+  const { task, dispatch } = useContext(TaskContext);
 
   const sortedTasks = sortTasks({field: 'startDate', direction: 'desc', tasks: task.tasks});
+
+  const hasTasks = task.tasks.length > 0;
 
   const tipo = {
     work: 'Foco',
     rest: 'Descanso Longo',
     shortRest: 'Descanso',
+  }
+
+  function handleResetHistory() {
+    if(!confirm('Tem certeza?')) return;
+
+    dispatch({type: TaskActionTypes.RESET_TASK});
   }
 
   return (
@@ -28,41 +37,49 @@ export function History() {
         <Container>
           <Heading>
             <span>History</span>
-            <span className={style.button}>
-              <ButtonDefault color="red" aria-label="Apagar todo o histórico" title="Apagar histórico">
-                <TrashIcon />
-              </ButtonDefault>
-            </span>
+            {hasTasks &&
+              <span className={style.button}>
+                <ButtonDefault color="red" aria-label="Apagar todo o histórico" title="Apagar histórico" onClick={handleResetHistory}>
+                  <TrashIcon />
+                </ButtonDefault>
+              </span> 
+            }
           </Heading>
         </Container>
 
         <Container>
-          <div className={style.responsiveTable}>
-            <table className={style.table}>
-              <thead>
-                <tr className={style.row}>
-                  <th>Tarefa</th>
-                  <th>Duração</th>
-                  <th>Data</th>
-                  <th>Status</th>
-                  <th>Tipo</th>
-                </tr>
-              </thead>
-              <tbody>
-                {sortedTasks.map((t) => {
-                  return (
-                    <tr key={t.id} className={`${style.row} ${style.bodyRow}`}>
-                      <td>{t.name}</td>
-                      <td>{t.durationInMinutes} min</td>
-                      <td>{formatDate(t.startDate)}</td>
-                      <td>{getTaskStatus(t, task.activeTask)}</td>
-                      <td>{tipo[t.type]}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+          { hasTasks && 
+            <div className={style.responsiveTable}>
+              <table className={style.table}>
+                <thead>
+                  <tr className={style.row}>
+                    <th>Tarefa</th>
+                    <th>Duração</th>
+                    <th>Data</th>
+                    <th>Status</th>
+                    <th>Tipo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {sortedTasks.map((t) => {
+                    return (
+                      <tr key={t.id} className={`${style.row} ${style.bodyRow}`}>
+                        <td>{t.name}</td>
+                        <td>{t.durationInMinutes} min</td>
+                        <td>{formatDate(t.startDate)}</td>
+                        <td>{getTaskStatus(t, task.activeTask)}</td>
+                        <td>{tipo[t.type]}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          }
+
+          { !hasTasks &&
+            <p className={style.emptyMessage}>Ainda não existem tarefas criadas.</p>
+          }
         </Container>
 
       </MainTemplate>
