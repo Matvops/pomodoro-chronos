b109264b1b0df801e0384865a6f034404427092c
Matheus <matheusmartinscia@gmail.com>
2026-05-08 22:51:14 -0300

    Feat: Adiciona método adapter confirm

--- src/adapters/showMessage.ts
+++ src/adapters/showMessage.ts
@@ -1,28 +1,43 @@
-import { toast } from "react-toastify";;
+import { toast } from "react-toastify"; import { Dialog } from "../components/Dialog";
+;
 
 export class showMessage {
 
-    static success(message?: string) {
-        toast.success(message ?? 'Success');
-    }
+  static success(message?: string) {
+    toast.success(message ?? 'Success');
+  }
 
-    static warn(message?: string) {
-        toast.warn(message ?? 'Warning');
-    }
+  static warn(message?: string) {
+    toast.warn(message ?? 'Warning');
+  }
 
-    static warning(message?: string) {
-        toast.warning(message ?? 'Warning');
-    }
+  static warning(message?: string) {
+    toast.warning(message ?? 'Warning');
+  }
 
-    static error(message?: string) {
-        toast.error(message ?? 'Error');
-    }
+  static error(message?: string) {
+    toast.error(message ?? 'Error');
+  }
 
-    static info(message?: string) {
-        toast.info(message ?? 'Error');
+  static info(message?: string) {
+    toast.info(message ?? 'Error');
+  }
+
+  static dismiss() {
+    toast.dismiss();
+  }
+
+  static confirm(data: string, onClosing: (confirmation: boolean) => void) {
+    toast(Dialog, {
+      data: data,
+      autoClose: false,
+      closeOnClick: false,
+      closeButton: false,
+      draggable: false,
+      onClose: confirmation => {
+        onClosing(confirmation === true);
+      }
     }
-    
-    static dismiss() {
-        toast.dismiss();
-    }
+    );
+  }
 }
